55029034c31f2f9d23d116395ad54599437639a5
Ivan Yakovlev <ivanvyakovlev@gamil.com>
2016-10-11 22:10:17 +0300

    Исправлены ошибки в соответствии с криетриями проекта

--- src/js/check.js
+++ src/js/check.js
@@ -1,31 +1,31 @@
 'use strict';
 
-function getMessage(a, b) {
-	if (typeof a === "boolean") {
-		if (a) {
-			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-		} else {
-			return "Переданное GIF-изображние не анимировано";
-		}
-	} else if (typeof a === "number") {
-		return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
-	} else if (Array.isArray(a) && Array.isArray(b)) {
-		var artifactsSquare = 0;
-		
-		for (var i = 0; i < a.length; i++) {
-			artifactsSquare += a[i] * b[i]
-		}
-				 
-		return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей"
-	} else if (Array.isArray(a)) {
-		var amountOfRedPoints = 0;
-			
-		for (i = 0; i < a.length; i++) {
-			amountOfRedPoints += a[i];
-		}
-			
-		return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-	} else {
-			return "Переданы некорректные данные"
-	} 
-}
+window.getMessage = (function(a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    } else {
+      return 'Переданное GIF-изображние не анимировано';
+    }
+  } else if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      artifactsSquare += a[i] * b[i];
+    }
+
+    return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  } else if (Array.isArray(a)) {
+    var amountOfRedPoints = 0;
+
+    for (i = 0; i < a.length; i++) {
+      amountOfRedPoints += a[i];
+    }
+
+    return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+  } else {
+    return 'Переданы некорректные данные';
+  }
+})();
